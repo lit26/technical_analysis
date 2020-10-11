3bfb654679ba8bc7e1be66c54972e6a2ef406c90
Tianning Li <ltianningli@gmail.com>
2020-10-10 22:01:57 -0700

    update pivot_point

--- pivot_point.py
+++ pivot_point.py
@@ -1,10 +1,10 @@
-# timeframe = daily
 import pandas as pd
 df = pd.read_csv('data/NIO_D.csv')
 df['Date'] = pd.to_datetime(df['Date'])
 
 timeframe = 'D'
 resample = None
+method = 'traditional'
 if timeframe == 'D':
     df['Last'] = df['Date'] - pd.DateOffset(months=1)
     resample = 'M'
@@ -22,25 +22,65 @@ df_close = df['Close'].resample(resample).last().reset_index().rename(columns={'
 df2 = df_low.merge(df_high,on='Date').merge(df_close,on='Date')
 df2 = df2[1:]
 
-# Traditional
-df2['PP'] = (df2['HighPrev'] + df2['LowPrev'] + df2['ClosePrev']) / 3
-df2['R1'] = df2['PP'] * 2 - df2['LowPrev']
-df2['S1'] = df2['PP'] * 2 - df2['HighPrev']
-df2['R2'] = df2['PP'] + (df2['HighPrev'] - df2['LowPrev'])
-df2['S2'] = df2['PP'] - (df2['HighPrev'] - df2['LowPrev'])
-df2['R3'] = df2['PP'] * 2 + (df2['HighPrev'] - 2 * df2['LowPrev'])
-df2['S3'] = df2['PP'] * 2 - (2 * df2['HighPrev'] - df2['LowPrev'])
-df2['R4'] = df2['PP'] * 3 + (df2['HighPrev'] - 3 * df2['LowPrev'])
-df2['S4'] = df2['PP'] * 3 - (3 * df2['HighPrev'] - df2['LowPrev'])
-df2['R5'] = df2['PP'] * 4 + (df2['HighPrev'] - 4 * df2['LowPrev'])
-df2['S5'] = df2['PP'] * 4 - (4 * df2['HighPrev'] - df2['LowPrev'])
+if method == 'traditional':
+    # Traditional
+    df2['PP'] = (df2['HighPrev'] + df2['LowPrev'] + df2['ClosePrev']) / 3
+    df2['R1'] = df2['PP'] * 2 - df2['LowPrev']
+    df2['S1'] = df2['PP'] * 2 - df2['HighPrev']
+    df2['R2'] = df2['PP'] + (df2['HighPrev'] - df2['LowPrev'])
+    df2['S2'] = df2['PP'] - (df2['HighPrev'] - df2['LowPrev'])
+    df2['R3'] = df2['PP'] * 2 + (df2['HighPrev'] - 2 * df2['LowPrev'])
+    df2['S3'] = df2['PP'] * 2 - (2 * df2['HighPrev'] - df2['LowPrev'])
+    df2['R4'] = df2['PP'] * 3 + (df2['HighPrev'] - 3 * df2['LowPrev'])
+    df2['S4'] = df2['PP'] * 3 - (3 * df2['HighPrev'] - df2['LowPrev'])
+    df2['R5'] = df2['PP'] * 4 + (df2['HighPrev'] - 4 * df2['LowPrev'])
+    df2['S5'] = df2['PP'] * 4 - (4 * df2['HighPrev'] - df2['LowPrev'])
+elif method == 'fibonacci':
+    # Fibonacci
+    df2['PP'] = (df2['HighPrev'] + df2['LowPrev'] + df2['ClosePrev']) / 3
+    df2['R1'] = df2['PP'] + 0.382 * (df2['HighPrev'] - df2['LowPrev'])
+    df2['S1'] = df2['PP'] - 0.382 * (df2['HighPrev'] - df2['LowPrev'])
+    df2['R2'] = df2['PP'] + 0.618 * (df2['HighPrev'] - df2['LowPrev'])
+    df2['S2'] = df2['PP'] - 0.618 * (df2['HighPrev'] - df2['LowPrev'])
+    df2['R3'] = df2['PP'] + (df2['HighPrev'] - df2['LowPrev'])
+    df2['S3'] = df2['PP'] - (df2['HighPrev'] - df2['LowPrev'])
+elif method == 'classic':
+    # Classic
+    df2['PP'] = (df2['HighPrev'] + df2['LowPrev'] + df2['ClosePrev']) / 3
+    df2['R1'] = 2 * df2['PP'] - df2['LowPrev']
+    df2['S1'] = 2 * df2['PP'] - df2['HighPrev']
+    df2['R2'] = df2['PP'] + (df2['HighPrev'] - df2['LowPrev'])
+    df2['S2'] = df2['PP'] - (df2['HighPrev'] - df2['LowPrev'])
+    df2['R3'] = df2['PP'] + 2 * (df2['HighPrev'] - df2['LowPrev'])
+    df2['S3'] = df2['PP'] - 2 * (df2['HighPrev'] - df2['LowPrev'])
+    df2['R4'] = df2['PP'] + 3 * (df2['HighPrev'] - df2['LowPrev'])
+    df2['S4'] = df2['PP'] - 3 * (df2['HighPrev'] - df2['LowPrev'])
+elif method == 'camarilla':
+    # Camarilla
+    df2['PP'] = (df2['HighPrev'] + df2['LowPrev'] + df2['ClosePrev']) / 3
+    df2['R1'] = df2['ClosePrev'] + 1.1 * (df2['HighPrev'] - df2['LowPrev']) / 12
+    df2['S1'] = df2['ClosePrev'] - 1.1 * (df2['HighPrev'] - df2['LowPrev']) / 12
+    df2['R2'] = df2['ClosePrev'] + 1.1 * (df2['HighPrev'] - df2['LowPrev']) / 6
+    df2['S2'] = df2['ClosePrev'] - 1.1 * (df2['HighPrev'] - df2['LowPrev']) / 6
+    df2['R3'] = df2['ClosePrev'] + 1.1 * (df2['HighPrev'] - df2['LowPrev']) / 4
+    df2['S3'] = df2['ClosePrev'] - 1.1 * (df2['HighPrev'] - df2['LowPrev']) / 4
+    df2['R4'] = df2['ClosePrev'] + 1.1 * (df2['HighPrev'] - df2['LowPrev']) / 2
+    df2['S4'] = df2['ClosePrev'] - 1.1 * (df2['HighPrev'] - df2['LowPrev']) / 2
+
+
+if timeframe == 'D':
+    left_join = [df['Last'].dt.year, df['Last'].dt.month]
+    right_join = [df2['Date'].dt.year, df2['Date'].dt.month]
+
+elif timeframe == 'W' or timeframe == 'M':
+    left_join = [df['Last'].dt.year]
+    right_join = [df2['Date'].dt.year]
 
 left_join = [df['Last'].dt.year,df['Last'].dt.month]
 right_join = [df2['Date'].dt.year,df2['Date'].dt.month]
 
-df = df.merge(df2, how='left', left_on=left_join, right_on=right_join)
+df2 = df.merge(df2, how='left', left_on=left_join, right_on=right_join)
+df2 = df2[cols]
+df2 = df2.rename(columns={"Date_x": "Date"})
 
-df_result = df[cols]
-
-print(df_result)
-# df_result.to_csv('result2.csv')
+print(df2)
